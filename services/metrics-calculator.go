845a023d7ddd526054e8f64d045bbc3cce79a711
Bruno Grassano <bgrassano@fi.uba.ar>
2024-03-20 19:01:55 -0300

    Add forecasted metrics

--- services/metrics-calculator.go
+++ services/metrics-calculator.go
@@ -8,7 +8,55 @@ import (
 )
 
 func getMetricsForForecastedStream(data *responses.LastForecast, neededMetrics []entities.ConfiguredMetric) *[]dtos.MetricCard {
-	return nil
+	if len(neededMetrics) == 0 {
+		return nil
+	}
+	var metrics []dtos.MetricCard
+	setUpFirstValues := false
+	var minValue float64
+	var maxValue float64
+	sumOfValues := 0.0
+	validValues := data.GetMainForecast()
+	totalValues := len(validValues)
+	for _, value := range validValues {
+		if !setUpFirstValues {
+			minValue = value
+			maxValue = value
+			setUpFirstValues = true
+		}
+		if minValue > value {
+			minValue = value
+		}
+		if value > maxValue {
+			maxValue = value
+		}
+		sumOfValues += value
+		validValues = append(validValues, value)
+	}
+
+	for _, metric := range neededMetrics {
+		metricName := entities.MapMetricToString(metric.MetricId)
+		metricValue := 0.0
+		if metric.MetricId == entities.Mediana {
+			sort.Float64s(validValues)
+			middle := totalValues / 2
+			if len(validValues)%2 == 0 {
+				metricValue = (validValues[middle-1] + validValues[middle]) / 2
+			} else {
+				metricValue = validValues[middle]
+			}
+		} else if metric.MetricId == entities.Maximo {
+			metricValue = maxValue
+		} else if metric.MetricId == entities.Minimo {
+			metricValue = minValue
+		} else if metric.MetricId == entities.Media {
+			metricValue = sumOfValues / float64(totalValues)
+		}
+		metrics = append(metrics, dtos.NewMetricCard(metricName, metricValue))
+	}
+
+	metrics = append(metrics, dtos.NewMetricCard(entities.MapMetricToString(entities.Observaciones), float64(totalValues)))
+	return &metrics
 }
 
 func getMetricsForObservedOrCuratedStream(data []responses.ObservedDataResponse, neededMetrics []entities.ConfiguredMetric) *[]dtos.MetricCard {
