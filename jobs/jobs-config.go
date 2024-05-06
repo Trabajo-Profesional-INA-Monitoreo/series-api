34e829d44ade8508e6c90ad287f299a17344dace
Bruno Grassano <bgrassano@fi.uba.ar>
2024-05-06 18:28:07 -0300

    Ref cron init

--- jobs/jobs-config.go
+++ jobs/jobs-config.go
@@ -2,11 +2,19 @@ package jobs
 
 import (
 	"github.com/Trabajo-Profesional-INA-Monitoreo/series-api/config"
+	"github.com/robfig/cron"
 	log "github.com/sirupsen/logrus"
+	"time"
 )
 
 func SetUpJobs(repositories *config.Repositories, apiConfig *config.ServiceConfigurationData) {
 	log.Infof("Setting up jobs")
-	SetUpFaultDetectorJobs(repositories, apiConfig)
-	SetUpNotificationsJobs(apiConfig)
+	customLocation, err := time.LoadLocation("America/Argentina/Buenos_Aires")
+	if err != nil {
+		log.Fatalf("Error starting notifications service, stopping... | Err: %v", err)
+	}
+	c := cron.NewWithLocation(customLocation)
+	SetUpFaultDetectorJobs(repositories, apiConfig, c)
+	SetUpNotificationsJobs(apiConfig, c)
+	c.Start()
 }
